23506ec422e9e6bb736c524f795d46e89b6b2fcd
CTokkie <32687510+CTokkie@users.noreply.github.com>
2017-10-10 14:17:08 -0700

    Swift Initial Commit
    - Went over two different versions of how to use a for loop

---
+++ Swift/Week2Practice.swift
@@ -0,0 +1,17 @@
+// For loop in swift  (lol started complining at 12:45)
+// 
+// for i in 1...10{
+// 	print( "Displaying the i in a for loop in \(i)" )
+// }
+
+// var start:Int = 5
+// var stop:Int = 10
+// for A in start..<stop	{
+// 	print( "Displaying the A in a for loop in \(A) using variables" )
+// }
+
+// for loop, setting the increment
+
+for i in stride(from: 0,to: 11, by: 2){
+	print ("Changing the increment \(i)")
+}
